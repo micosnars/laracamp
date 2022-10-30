cff5ac183764a478cb49f9d2e304c2ca2d325bf2
micosnars <mikosnars@gmail.com>
2022-10-30 16:05:36 +0700

    slicing landing page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('_layouts.app')
+@section('content*)
+     <section class="banner">
+     </section>
+
+     <section class="benefits">
+     </section>
+
+     <section class="steps">
+     </section>
+
+     <section class="pricing">
+     </section>
+     
+     <section class="testimonials">
+     </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('login', function () {
+    return view('auth.user.login');
+});
+Route::get('checkout/{camp}', function () {
+    return view('checkout.create');
+});
+Route::get('checkout/{success}', function () {
+    return view('checkout.success');
+});
