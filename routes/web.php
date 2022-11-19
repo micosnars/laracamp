7f5f5ccbc2b2c007f2944e4c1791cd96a3d6ffd6
micosnars <mikosnars@gmail.com>
2022-11-19 17:48:38 +0700

    oauth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('login', function () {
-    return view('login');
-})->name('login');
-
 Route::get('checkout', function () {
     return view('checkout');
 })->name('checkout');
@@ -28,3 +26,19 @@ Route::get('checkout', function () {
 Route::get('success-checkout', function () {
     return view('success_checkout');
 })->name('success-checkout');
+
+// socialite routes
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
